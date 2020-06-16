916d09ba1dcccc596d1d1a2b6b4f69f3f7fab8cc
joshua miller <joshua.miller@student.centralia.edu>
2020-06-16 13:09:40 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_ComicBooksCharacters.Models;
+
+namespace MVC_ComicBooksCharacters.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ComicBookContext cbtxt = null;
+        public HomeController(ComicBookContext txt)
+        {
+            cbtxt = txt;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(cbtxt);
+        }
+    }
+}
---
+++ Models/ComicBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_ComicBooksCharacters.Models
+{
+    public class ComicBook
+    {
+        public int ComicBookID { get; set; }
+        public string ComicBookTitle { get; set; }
+        public string ComicBookDescription { get; set; }
+        public string ComicBookImage { get; set; }
+    }
+}
